a7ed76bc084698504fe4fced1ad616e53562317f
goutham123-dotcom <55642670+goutham123-dotcom@users.noreply.github.com>
2023-05-24 14:30:37 +0530

    Add files via upload

---
+++ src/hooks/useDocTitle.js
@@ -0,0 +1,15 @@
+import { useEffect } from 'react';
+
+const useDocTitle = (title) => {
+    useEffect(() => {
+        if (title) {
+            document.title = `${title} - Audiofyed`;
+        } else {
+            document.title = 'Audiofyed | The Perfect Audio Store';
+        }
+    }, [title]);
+
+    return null;
+};
+
+export default useDocTitle;
